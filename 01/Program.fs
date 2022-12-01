d530e82cbe81ea776779c8e411d1a7ce219be2d6
grajekf <filip.grajek@gmail.com>
2022-12-01 12:13:21 +0100

    Solution for 1b

--- 01/Program.fs
+++ 01/Program.fs
@@ -3,15 +3,29 @@ open System
 
 let inputText = File.ReadAllText("inputa.txt")
 
-printfn "%s" (
+let allSums = (
     inputText.Split("\r\n\r\n") 
-        |> Array.toList 
-        |> List.map(fun x -> (
-            x.Split("\r\n", StringSplitOptions.RemoveEmptyEntries ||| StringSplitOptions.TrimEntries) 
-                |> Array.toList 
-                |> List.map(int) 
-                |> List.sum)
-            ) 
-        |> List.max
-        |> string
+    |> Array.toList 
+    |> List.map(fun x -> (
+        x.Split("\r\n", StringSplitOptions.RemoveEmptyEntries ||| StringSplitOptions.TrimEntries) 
+            |> Array.toList 
+            |> List.map(int) 
+            |> List.sum)
+        ) 
+)
+
+//1a
+printfn "%s" (
+    allSums
+    |> List.max
+    |> string
+)
+
+//1b
+printfn "%s" (
+    allSums
+    |> List.sortDescending
+    |> Seq.take 3
+    |> Seq.sum
+    |> string
 )
