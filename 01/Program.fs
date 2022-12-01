8be78835de18e78e87a42710c050b165c691da97
grajekf <filip.grajek@gmail.com>
2022-12-01 12:06:26 +0100

    solution for 1a

---
+++ 01/Program.fs
@@ -0,0 +1,17 @@
+﻿open System.IO
+open System
+
+let inputText = File.ReadAllText("inputa.txt")
+
+printfn "%s" (
+    inputText.Split("\r\n\r\n") 
+        |> Array.toList 
+        |> List.map(fun x -> (
+            x.Split("\r\n", StringSplitOptions.RemoveEmptyEntries ||| StringSplitOptions.TrimEntries) 
+                |> Array.toList 
+                |> List.map(int) 
+                |> List.sum)
+            ) 
+        |> List.max
+        |> string
+)
