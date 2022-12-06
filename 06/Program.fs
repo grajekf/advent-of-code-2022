c96729d8c9e37a107a407d15f820e9d68971fde6
grajekf <filip.grajek@gmail.com>
2022-12-06 17:23:19 +0100

    Solution for 6

---
+++ 06/Program.fs
@@ -0,0 +1,11 @@
+﻿open System.IO
+
+let inputText = File.ReadAllText("input.txt")
+
+let startOfPacket = inputText |> List.ofSeq |> List.windowed 4 |> List.findIndex (fun window -> (window |> List.distinct |> List.length) = 4)
+
+printfn "%d" (startOfPacket + 4)
+
+let startOfMessage = inputText |> List.ofSeq |> List.windowed 14 |> List.findIndex (fun window -> (window |> List.distinct |> List.length) = 14)
+
+printfn "%d" (startOfMessage + 14)
